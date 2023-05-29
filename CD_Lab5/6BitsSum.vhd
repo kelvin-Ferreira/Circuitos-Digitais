b84db2e95ffa739d6904c59f3f46fdac08339ed7
kelvin-Ferreira <113177664+kelvin-Ferreira@users.noreply.github.com>
2023-05-29 08:53:02 -0300

    Add files via upload

--- CD_Lab5/6BitsSum.vhd
+++ CD_Lab5/6BitsSum.vhd
@@ -8,12 +8,11 @@ END;
 
 ARCHITECTURE behav OF Sum6b IS
 	SIGNAL co5, co4, co3, co2, co1, co0: BIT
-	SIGNAL cob5, cob4, cob3, cob2, cob1, cob0: BIT
-	SIGNAL Z5, Z4, Z3, Z2, Z1, Z0: BIT
+    SIGNAL Ba5, Ba4, Ba3, Ba2, Ba1, Ba0: BIT
 	
-    COMPONENT Mux2x1_d is
-        PORT(I0_comp,I1_comp,s0_comp : IN BIT; 
-        d_comp : OUT BIT);
+    COMPONENT Mux2x1_6bits is
+        PORT(Bc0, Bc1, Bc2, Bc3, Bc4, Bc5, Sc0 : IN BIT; 
+    d0, d1, d2, d3, d4, d5 : OUT BIT);
     END COMPONENT;
 
     COMPONENT hSum is
@@ -27,24 +26,17 @@ ARCHITECTURE behav OF Sum6b IS
     END COMPONENT;
 
 BEGIN
-   U6: Sum PORT MAP(A <= A5, B <= B5, ci <= co4, co <= co5, S <= S5); 
-	U5: Sum PORT MAP(A <= A4, B <= B4, ci <= co3, co <= co4, S <= S4); 
-	U4: Sum PORT MAP(A <= A3, B <= B3, ci <= co2, co <= co3, S <= S3); 
-	U3: Sum PORT MAP(A <= A2, B <= B2, ci <= co1, co <= co2, S <= S2); 
-	U2: Sum PORT MAP(A <= A1, B <= B1, ci <= co0, co <= co1, S <= S1); 
-	U1: hSum PORT MAP(A <= A0, B <= B0, co <= co0, S <= S0);
-	
-	U12: Sum PORT MAP(A <= A5, NOT(B) <= B5, ci <= cob4, co <= cob5, S <= Z5); 
-	U11: Sum PORT MAP(A <= A4, NOT(B) <= B4, ci <= cob3, co <= cob4, S <= Z4); 
-	U10: Sum PORT MAP(A <= A3, NOT(B) <= B3, ci <= cob2, co <= cob3, S <= Z3); 
-	U9: Sum PORT MAP(A <= A2, NOT(B) <= B2, ci <= cob1, co <= cob2, S <= Z2); 
-	U8: Sum PORT MAP(A <= A1, NOT(B) <= B1, ci <= cob0, co <= cob1, S <= Z1); 
-	U7: hSum PORT MAP(A <= A0, NOT(B) <= B0, co <= cob0, S <= Z0);
-	
-	U18: Sum PORT MAP(A <= A5, '1' <= B5, ci <= cob4, co <= cob5, S <= Z5); 
-	U17: Sum PORT MAP(A <= A4, '1' <= B4, ci <= cob3, co <= cob4, S <= Z4); 
-	U16: Sum PORT MAP(A <= A3, '1' <= B3, ci <= cob2, co <= cob3, S <= Z3); 
-	U15: Sum PORT MAP(A <= A2, '1' <= B2, ci <= cob1, co <= cob2, S <= Z2); 
-	U14: Sum PORT MAP(A <= A1, '1' <= B1, ci <= cob0, co <= cob1, S <= Z1); 
-	U13: hSum PORT MAP(A <= A0, '1' <= B0, co <= cob0, S <= Z0);
+    
+    U0: Mux2x1_6bits PORT MAP(Bc0 <= B0, Bc1 <= B1, Bc2 <= B2, Bc3 <= B3,
+        Bc4 <= B4, Bc5 <= B5, Sc0 <= sub, d0 <= Ba0, d1 <= Ba1, d2 <= Ba2,
+        d3 <= Ba3, d4 <= Ba4, d5 <= Ba5); 
+    U6: Sum PORT MAP(A <= A5, B <= Ba5, ci <= co4  OR sub, co <= co5, S <= S5); 
+	U5: Sum PORT MAP(A <= A4, B <= Ba4, ci <= co3 OR sub, co <= co4, S <= S4); 
+	U4: Sum PORT MAP(A <= A3, B <= Ba3, ci <= co2 OR sub, co <= co3, S <= S3); 
+	U3: Sum PORT MAP(A <= A2, B <= Ba2, ci <= co1 OR sub, co <= co2, S <= S2); 
+	U2: Sum PORT MAP(A <= A1, B <= Ba1, ci <= co0 OR sub, co <= co1, S <= S1); 
+	U1: hSum PORT MAP(A <= A0, B <= Ba0, co <= co0, S <= S0);
 END;
+
+PROCESS()
+        BEGIN
---
+++ CD_Lab5/Mux2x1_6Bits.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY Mux2x1_6bits IS
+    PORT(Bc0, Bc1, Bc2, Bc3, Bc4, Bc5, Sc0 : IN BIT; 
+    d0, d1, d2, d3, d4, d5 : OUT BIT);
+END;
+
+ARCHITECTURE behav OF Mux2x1_6bits IS
+    COMPONENT Mux2x1_d is
+        PORT(I0_comp,I1_comp,s0_comp : IN BIT;
+        d_comp : OUT BIT);
+    END COMPONENT;
+BEGIN
+    U1: Mux2x1_d PORT MAP(I0_comp => Bc0, I1_comp => NOT(Bc0), s0_comp => sc0, d_comp => d0); 
+    U2: Mux2x1_d PORT MAP(I0_comp => Bc1, I1_comp => NOT(Bc1), s0_comp => sc0, d_comp => d1); 
+    U3: Mux2x1_d PORT MAP(I0_comp => Bc2, I1_comp => NOT(Bc2), s0_comp => sc0, d_comp => d2);
+	U4: Mux2x1_d PORT MAP(I0_comp => Bc3, I1_comp => NOT(Bc3), s0_comp => sc0, d_comp => d3);
+    U5: Mux2x1_d PORT MAP(I0_comp => Bc3, I1_comp => NOT(Bc4), s0_comp => sc0, d_comp => d4);
+    U6: Mux2x1_d PORT MAP(I0_comp => Bc3, I1_comp => NOT(Bc5), s0_comp => sc0, d_comp => d5);
+END;
