8d5850e83fdde2fc62b73d3ccf1ea25ad7a07af9
kelvin-Ferreira <113177664+kelvin-Ferreira@users.noreply.github.com>
2023-06-30 10:16:25 -0300

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -6,7 +6,7 @@ use ieee.std_logic_1164.all;
 
 ENTITY ALU IS
 
-    PORT(clk, e, xn, yn, zn, cin: IN BIT;
+    PORT(clk, e, xn, yn, zn: IN BIT;
 
 	 An: IN BIT_VECTOR(5 DOWNTO 0); 
 
@@ -21,7 +21,7 @@ END;
 ARCHITECTURE behav OF ALU IS
 
     CONSTANT M : BIT := '0';
-    SIGNAL Z0 : BIT;
+    SIGNAL Z0, Z4 : BIT;
     SIGNAL Z1 :BIT_VECTOR(5 DOWNTO 0);
     SIGNAL Z2 :BIT_VECTOR(5 DOWNTO 0);
     SIGNAL Z3 :BIT_VECTOR(5 DOWNTO 0);
@@ -61,8 +61,8 @@ ARCHITECTURE behav OF ALU IS
 
 BEGIN
 
-	U1: AL PORT MAP(x => xn, y => yn, z => zn, A => An, B => Bn, Icin => cin, IA => Z1, IB => Z2);
-	U2: Sum6b PORT MAP(A6 => Z1, B6 => Z2, sub => M, S6 => Z3);
+	U1: AL PORT MAP(x => xn, y => yn, z => zn, A => An, B => Bn, Icin => Z4, IA => Z1, IB => Z2);
+	U2: Sum6b PORT MAP(A6 => Z1, B6 => Z2, sub => Z4, S6 => Z3);
 	U3: reg6b PORT MAP(clk_R => clk, ld => e, D => Z3, Q => S);
 
 END;
