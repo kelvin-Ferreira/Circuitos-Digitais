72da0117e1ab530aed9f038d7017423d61d9d62c
kelvin-Ferreira <113177664+kelvin-Ferreira@users.noreply.github.com>
2023-06-30 10:49:10 -0300

    Update AL.vhd

--- AL.vhd
+++ AL.vhd
@@ -14,5 +14,5 @@ ARCHITECTURE behav OF AL IS
 BEGIN
     Icin = (NOT(x) AND NOT(y) AND z) OR (NOT(x) AND y AND NOT(z));
     IA = (A AND(NOT(x AND y AND z) OR NOT(x AND y AND NOT(z)) OR NOT(x AND NOT(Y) AND NOT(z)) OR NOT(x AND NOT(Y) AND z))) OR (NOT(A) AND (x AND y AND z)) OR ((A OR B) AND (x AND y AND NOT(z))) OR ((A OR B) AND (x AND z AND NOT(y))) OR (A AND B AND x AND NOT(y) AND NOT(z))
-    IB = (B AND (NOT(x) AND NOT(y) AND NOT(z))) OR (B AND (NOT(x) AND NOT(y) AND z))
+    IB = (B AND (NOT(x) AND NOT(y) AND NOT(z))) OR (NOT(B) AND (NOT(x) AND NOT(y) AND z))
 END;
